f6e504c7a42a4933b861ca0c2dacba682a512ce7
hyeonwook98 <76507547+hyeonwook98@users.noreply.github.com>
2022-07-05 18:06:14 +0900

    프로젝트 시작

---
+++ valuetogether/valuetogether/valuetogether/src/main/java/kakao/valuetogether/ValuetogetherApplication.java
@@ -0,0 +1,13 @@
+package kakao.valuetogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValuetogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValuetogetherApplication.class, args);
+	}
+
+}
---
+++ valuetogether/valuetogether/valuetogether/src/test/java/kakao/valuetogether/ValuetogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package kakao.valuetogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValuetogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
