bdda72fb88e0a36e504817f46874eef4ea7e2d69
hyeonwook98 <hyeonwook98@naver.com>
2022-07-05 18:22:12 +0900

    test: branch테스트

--- valuetogether/valuetogether/valuetogether/src/main/java/kakao/valuetogether/ValuetogetherApplication.java
+++ valuetogether/valuetogether/valuetogether/src/main/java/kakao/valuetogether/ValuetogetherApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ValuetogetherApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ValuetogetherApplication.class, args);
+		System.out.println("asdf");
 	}
 
 }
