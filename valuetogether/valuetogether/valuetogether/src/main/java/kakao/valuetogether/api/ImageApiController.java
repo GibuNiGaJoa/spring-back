66b23759f3fa01cbbc99e15772046d8b7d1d38f8
DongHo Kim <hkim4410@naver.com>
2022-08-03 11:36:21 +0900

    merge 전 commit

--- valuetogether/valuetogether/valuetogether/src/main/java/kakao/valuetogether/api/ImageApiController.java
+++ valuetogether/valuetogether/valuetogether/src/main/java/kakao/valuetogether/api/ImageApiController.java
@@ -1,25 +1,28 @@
 package kakao.valuetogether.api;
 
-import kakao.valuetogether.service.CommentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.annotation.Resource;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-
-// String 받는 메소드 -> base64 추출하는 메소드 -> base64를 저장하고 URL 반환하는 메소드
 
 @RestController
 @RequiredArgsConstructor
-public class CommentApiController {
+public class ImageApiController {
 
-    private final CommentService commentService;
+    @PostMapping("/test")
+    public String upload(@RequestParam("img") MultipartFile imageFile) throws IOException {
+        imageFile.transferTo(new File("C:\\Users\\kimdongho\\Desktop\\spring-back\\valuetogether\\valuetogether\\valuetogether\\src\\main\\resources\\static\\image\\" + imageFile.getOriginalFilename()));
+        return "http://localhost:8080/image/" + imageFile.getOriginalFilename();
 
-//    @PostMapping("/test")
+//        imageFile.transferTo(new File("/home/ubuntu/app/valuetogether/valuetogether/valuetogether/src/main/resources/static/image" + imageFile.getOriginalFilename()));
+//        return "http://valuetogether.tk/image/" + imageFile.getOriginalFilename();
+    }
+
+    //    @PostMapping("/test")
 //    public String saveProfileImage(MultipartFile imageFile) throws Exception {
 //        String imagePath = null;
 //        String absolutePath = new File("").getAbsolutePath() + "\\";
@@ -58,13 +61,4 @@ public class CommentApiController {
 //
 //        return imagePath;
 //    }
-
-    @PostMapping("/test")
-    public String upload(@RequestParam("img") MultipartFile imageFile) throws IOException {
-//        imageFile.transferTo(new File("C:\\Users\\kimdongho\\Desktop\\spring-back\\valuetogether\\valuetogether\\valuetogether\\src\\main\\resources\\static\\image\\" + imageFile.getOriginalFilename()));
-//        return "http://localhost:8080/image/" + imageFile.getOriginalFilename();
-
-        imageFile.transferTo(new File("/home/ubuntu/app/valuetogether/valuetogether/valuetogether/src/main/resources/static/image" + imageFile.getOriginalFilename()));
-        return "http://valuetogether.tk/image/" + imageFile.getOriginalFilename();
-    }
 }
