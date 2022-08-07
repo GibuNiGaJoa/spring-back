7e55be0b1b39af1ddbb9cc54a1df8aa279dd4c5c
hyeonwook98 <hyeonwook98@naver.com>
2022-08-07 20:44:49 +0900

    test: api테스트

--- valuetogether/valuetogether/valuetogether/src/main/java/kakao/valuetogether/api/PostApiController.java
+++ valuetogether/valuetogether/valuetogether/src/main/java/kakao/valuetogether/api/PostApiController.java
@@ -2,52 +2,49 @@ package kakao.valuetogether.api;
 
 import kakao.valuetogether.domain.Member;
 import kakao.valuetogether.domain.Post;
-
-import kakao.valuetogether.service.*;
-
+import kakao.valuetogether.service.MemberService;
+import kakao.valuetogether.service.PostService;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-
 
 @RestController // @Controller + @ResponseBody가 이 어노테이션에 포함된다.
 @RequiredArgsConstructor
 public class PostApiController {
 
     private final PostService postService;
+
     private final MemberService memberService;
 
-    //기부 제안하기
     @PostMapping("/propose/project")
-    public ProposeResponse propose(@RequestBody @Valid ProposeRequest request){
-
+    public CreatedPostResponse proposePost(@RequestBody @Valid CreatedPostRequest request) {
+        Member findMember = memberService.findOne(1L);
         Post post = new Post();
+        post.setMember(findMember);
         post.setTitle(request.getA());
         post.setSubTitle(request.getB());
         post.setContent(request.getC());
 
-        Member findMember = memberService.findOne(1L);
-        post.setMember(findMember);
-
         postService.propose(post);
-
-        return new ProposeResponse(true);
+        return new CreatedPostResponse(true);
     }
+
     @Data
-    static class ProposeRequest {
+    static class CreatedPostRequest {
         private String a;
-
         private String b;
-
         private String c;
     }
+
     @Data
-    static class ProposeResponse {
+    static class CreatedPostResponse {
         private Boolean status;
 
-        public ProposeResponse(Boolean status) {
+        public CreatedPostResponse(Boolean status) {
             this.status = status;
         }
     }
