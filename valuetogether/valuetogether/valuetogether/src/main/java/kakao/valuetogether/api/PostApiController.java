06d8847cb719dbdccc1c75cb2908947e5ce9709a
hyeonwook98 <hyeonwook98@naver.com>
2022-08-24 16:50:22 +0900

    chore: 좋아요 api 개발

--- valuetogether/valuetogether/valuetogether/src/main/java/kakao/valuetogether/api/PostApiController.java
+++ valuetogether/valuetogether/valuetogether/src/main/java/kakao/valuetogether/api/PostApiController.java
@@ -117,7 +117,7 @@ public class PostApiController {
 
     //게시글 정보 조회
     @GetMapping("fundraisings/{id}")
-    public FindPostResponse findPost(@RequestHeader(value = "Authorization") @RequestHeaderNonNull String token, @PathVariable("id") Long id) {
+    public FindPostResponse findPost(@RequestHeader(value = "Authorization") String token, @PathVariable("id") Long id) {
         Post findPost = postService.findOneById(id);
 
         List<Tag> findTags = tagPostService.findTagByPost(findPost);
--- valuetogether/valuetogether/valuetogether/src/main/java/kakao/valuetogether/validation/RequestHeaderNonNull.java
+++
@@ -1,9 +0,0 @@
-package kakao.valuetogether.validation;
-
-import java.lang.annotation.*;
-
-@Target(ElementType.PARAMETER)
-@Retention(RetentionPolicy.RUNTIME)
-@Documented
-public @interface RequestHeaderNonNull {
-}
